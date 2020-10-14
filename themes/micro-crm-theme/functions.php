9225ebd3bd8581995862994bee5114067d85ef9d
namwood <namwood2019@gmail.com>
2020-10-13 23:13:27 -0400

    Initial commit

---
+++ themes/micro-crm-theme/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+// Load CSS files
+function load_css(){
+  wp_register_style('stylesheet', get_template_directory_uri() . '/src/css/main.css');
+  wp_enqueue_style('stylesheet');
+
+  wp_register_style('another_css', get_template_directory_uri() . '/style.css');
+  wp_enqueue_style('another_css');
+}
+
+add_action('wp_enqueue_scripts', 'load_css');
+
+// Load Javascript files
+function load_javascript(){
+  wp_register_script('app-javascript', get_template_directory_uri() . '/src/js/app.js', 'jquery', 1, true);
+  wp_enqueue_script('app-javascript');
+}
+
+add_action('wp_enqueue_scripts', 'load_javascript');
