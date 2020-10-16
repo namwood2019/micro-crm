50a03b39f15a8599ed11407e55ba51e0a0c5ef33
maddoushe <69418642+maddoushe@users.noreply.github.com>
2020-10-15 21:19:32 -0400

    Update contact.php

--- themes/micro-crm-theme/template-parts/contact.php
+++ themes/micro-crm-theme/template-parts/contact.php
@@ -9,17 +9,30 @@
  <!-- Section A: Contact Form -->
  <section id="contact-a" class="text-center py-3">
     <div class="container">
-      <h2 class="section-title">Contact</h2>
+      <h1 class="section-title">Contact</h1>
       <div class="bottom-line"></div>
-      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-      <br/>
+      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit 
+      Lorem ipsum dolor sit amet, 
+      </p>
+  
+      <div class="mapouter">
+        <div class="gmap_canvas">
+          <iframe width="700" height="300" id="gmap_canvas" src="https://maps.google.com/maps?q=cegep%20garneau&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+          </iframe>
+        </div>
+        <style>
+          .mapouter{position:relative;text-align:right;height:300px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}
+        </style>
+      </div>
+
+      <h2>Nous contacter</h2>
       <form>
         <div class="text-fields">
           <input type="text" class="text-input name-input" placeholder="Nom">
           <input type="text" class="text-input subject-input" placeholder="Sujet">
           <input type="text" class="text-input email-input" placeholder="Courriel">
           <input type="text" class="text-input phone-input" placeholder="Phone">
-          <textarea class="text-input message-input" placeholder="Message"></textarea>
+          <textarea class="text-input message-input" placeholder="Message"  rows="7"></textarea>
         </div>
         <button type="submit" class="btn-dark">Submit</button>
       </form>
@@ -31,15 +44,19 @@
     <div class="container">
       <div class="contact-info">
         <div>
-          <i class="fa fa-phone fa-2x"></i>
-          <h3>Phone</h3>
-          <p>+1 (418) 555-5555</p>
+          <a href="tel:555-555-5555">
+            <i class="fa fa-phone fa-2x"></i>
+            <h3>Phone</h3>
+            555-555-5555
+          </a>
         </div>
 
         <div>
-          <i class="fa fa-envelope fa-2x"></i>
-          <h3>Email</h3>
-          <p>info@vertlette.com</p>
+          <a href="mailto:name@email.com">
+            <i class="fa fa-envelope fa-2x"></i>
+            <h3>Email</h3>
+            send email
+          </a>
         </div>
 
         <div>
